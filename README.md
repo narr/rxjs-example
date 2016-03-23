354c759fdc3e4d29f7179fdca6e9b6a36e48a7cd
Narr <nardgu@gmail.com>
2016-03-23 13:28:11 +0900

    add mouse event animation

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # rxjs-example
 rxjs-example
+<br>Github page link: <https://narr.github.io/rxjs-example/>
---
+++ webpack.config.build.js
@@ -0,0 +1,75 @@
+const del = require('del');
+const path = require('path');
+const appPath = path.join(__dirname, 'js');
+const distPath = path.join(__dirname, 'public');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+del.sync(distPath);
+
+module.exports = {
+  context: appPath,
+  entry: {
+    // to set orders in HTML, add numbers to entry names
+    '0_jquery': ['jquery'],
+    '1_rxjs': ['rxjs'],
+    '2_main': ['./index.js']
+  },
+  output: {
+    path: distPath,
+    filename: 'js/[name].bundle.js?[hash]'
+  },
+  module: {
+    preLoaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loaders: ['eslint']
+    }],
+    loaders: [{
+      test: /\.js?$/,
+      exclude: /node_modules/,
+      loaders: ['babel']
+    }]
+  },
+  eslint: {
+    emitErrors: true,
+    failOnHint: true
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      // webpackjsonp is declared in the last name in names array
+      // so make this order inverse to the entry order
+      names: [
+        '1_rxjs',
+        '0_jquery'
+      ],
+      filename: 'js/[name].bundle.js?[hash]'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      minify: {
+        minifyCSS: true,
+        minifyJS: true,
+        removeComments: true,
+        collapseWhitespace: true,
+      },
+      template: path.join(appPath, '../index.html'),
+      inject: 'body',
+      chunksSortMode: 'none' // for js's orders in HTML
+    }),
+    new webpack.optimize.DedupePlugin(), // https://github.com/webpack/docs/wiki/optimization#deduplication
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        drop_console: true
+      }
+    }),
+    new CopyWebpackPlugin([
+      { from: '../css', to: 'css' } // from: relative path to context path
+    ])
+  ],
+  resolve: {
+    root: appPath
+  },
+  bail: true
+};
